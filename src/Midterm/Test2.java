460184214b87fee8b5a36864f9cbdf6bb8b42778
LAB_COM-MT_1 <kamonart.meen@gmail.com>
2018-01-16 11:31:27 +0700

    txtCulalate

--- src/Midterm/Test2.java
+++ src/Midterm/Test2.java
@@ -1,4 +1,48 @@
 package Midterm;
 
+import java.util.Scanner;
+
 public class Test2 {
-}
+    static Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+        String id = getID();
+        String Name = getName();
+        String Salary = getSalary();
+        String OverTime = getOverTime();
+        String calculateTax = calculateTax();
+        String showDataInfo = showDataInfo();
+
+    }//main
+       private static String getID() {
+        System.out.print("Enter employee id: ");
+        return scanner.nextLine();
+       }
+
+    private static String getName() {
+        System.out.print("Enter employee  Name: ");
+        return scanner.nextLine();
+    }
+
+    private static String getSalary() {
+        System.out.print("Enter employee Salary: ");
+        return scanner.nextLine();
+    }
+
+    private static String getOverTime() {
+        System.out.print("Enter employee OverTime: ");
+        return scanner.nextLine();
+    }
+
+    private static String calculateTax() {
+        System.out.print("Enter employee calculateTax: ");
+        return scanner.nextLine();
+    }
+
+    private static String showDataInfo() {
+        System.out.print("Enter employee showDataInfo: ");
+        return scanner.nextLine();
+    }
+
+
+
+}//class
